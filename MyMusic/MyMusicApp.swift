73b25c8b003e410dd169789ccf138b49b994944c
yanagyi <yanagyi429@gmail.com>
2024-07-09 01:33:49 +0900

    Initial Commit

---
+++ MyMusic/MyMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMusicApp.swift
+//  MyMusic
+//
+//  Created by Hiro Yanagisawa on 2024/07/09.
+//
+
+import SwiftUI
+
+@main
+struct MyMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
